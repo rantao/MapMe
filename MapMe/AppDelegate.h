7b8a370568438230f9273c8db4ecd689f4c7d723
Ran Tao <ran@ephexi.com>
2012-08-14 15:37:18 -0700

    Initial Commit

---
+++ MapMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapMe
+//
+//  Created by Ran Tao on 8.14.12.
+//  Copyright (c) 2012 Ran Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapMe
+//
+//  Created by Ran Tao on 8.14.12.
+//  Copyright (c) 2012 Ran Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
